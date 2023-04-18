f24ca945fb839983eb99927d273b45f37b89efcc
chutikarnkh <c.khumprom@gmail.com>
2023-04-18 18:12:15 +0700

    Add media query and mobile navigation

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center">
+  <img alt="Logo" src="https://raw.githubusercontent.com/chutikarnkh/chutikarnkh.github.io/e3ae43d2871d8b41ca218a984a8ed005b13b44ca/img/light_logo.svg" width="100" />
+</div>
+<h1 align="center">
+  chutikarnkh.github.io
+</h1>
+<p align="center">
+  My portfolio <a href="https://chutikarnkh.github.io/" target="_blank">chutikarnkh.github.io</a> built with HTML, JS, SASS and hosted with <a href="https://pages.github.com/" target="_blank">GitHub Pages</a>
+</p>
+<p align="center">
+  <a href="https://github.com/chutikarnkh/chutikarnkh.github.io/actions/workflows/pages/pages-build-deployment" target="_blank">
+    <img src="https://github.com/chutikarnkh/chutikarnkh.github.io/actions/workflows/pages/pages-build-deployment/badge.svg" alt="GitHub Deploy Status" />
+  </a>
+</p>
+
+![demo](https://raw.githubusercontent.com/chutikarnkh/chutikarnkh.github.io/main/img/projects/project2.png)
--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ const link = document.querySelectorAll('.main-nav-link');
 //==================================
 // Active link
 //==================================
-mainNav.addEventListener('click', (e) => {
-  if (e.target.classList.contains('main-nav-link')) {
-    removeActive();
-    e.target.classList.add('active');
-  }
-});
+// mainNav.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('main-nav-link')) {
+//     removeActive();
+//     e.target.classList.add('active');
+//   }
+// });
 
 
 function removeActive() {
@@ -19,6 +19,13 @@ function removeActive() {
   }
 }
 
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
 
 //==================================
 // Smooth scrolling animation
@@ -28,7 +35,6 @@ allLinks.forEach((link) => {
   link.addEventListener('click', (e) => {
 
     const href = link.getAttribute('href');
-
     // Scroll back to top
     if (href === '#') {
       e.preventDefault();
@@ -43,6 +49,13 @@ allLinks.forEach((link) => {
       e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+      removeActive();
+      link.classList.add('active');
     }
   });
 });
